1eeae4e761a93e8a62b436c2aa71cb86eef5764d
H00gy <dimitry.ross.hy@gmail.com>
2026-09-16 15:24:03 -0600

    Update ArrowMovement.cs

--- Assets/Scripts/ArrowMovement.cs
+++ Assets/Scripts/ArrowMovement.cs
@@ -8,6 +8,8 @@ public class ArrowMovement : MonoBehaviour
     public float distance; // How far it moves in pixels
     public float speed = 2f;      // How fast it moves
 
+    bool isMoving = true;
+
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
@@ -17,11 +19,25 @@ public class ArrowMovement : MonoBehaviour
     
     void Update()
     {
-        // PingPong creates a smooth looping value between 0 and distance * 2
-        float movement = Mathf.PingPong(Time.time * speed * distance, distance * 2) - distance;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isMoving = false;
+
+
+        }
+
 
         // Apply movment
-        rectTransform.anchoredPosition = new Vector2(startPos.x + movement, startPos.y);
+        if (isMoving)
+        {
+            // PingPong creates a smooth looping value between 0 and distance * 2
+            float movement = Mathf.PingPong(Time.time * speed * distance, distance * 2) - distance;
+            rectTransform.anchoredPosition = new Vector2(startPos.x + movement, startPos.y);
+        }
+        
+
+        
     }
     
 }
